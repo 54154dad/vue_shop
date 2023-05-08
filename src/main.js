be465e34259a8729d6b4cfcfcf3fa7eadc85579e
54154dad <2467883840@qq.com>
2023-05-08 15:54:39 +0800

    commit second

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router.js";
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
 Vue.config.productionTip = false
+Vue.use(MintUI)
 
 new Vue({
   router,
